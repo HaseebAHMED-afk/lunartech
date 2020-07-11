2564742986e1b7db13854ade2bbfbfda7cf35fea
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-07-11 23:05:55 +0500

    Fixed MainNav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -11,7 +11,7 @@ function MainNav(){
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="./page.html">Cloud Computing<span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="page.html">Cloud Computing<span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item active">
           <a className="nav-link" href="page.html">Artifical Intelligence</a>
